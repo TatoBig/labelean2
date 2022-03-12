8e403e7fd26422aab7edc64937510fc48c6c58ac
TatoBig <Tatobig@gmail.com>
2022-03-11 22:42:41 -0600

    Initial webpack config

---
+++ src/templates/Template.js
@@ -0,0 +1,24 @@
+import image from '../assets/image.png'
+
+const Template = async () => {
+  const view = `
+    <div class="About">
+      <div class="card">
+        <div class="card_social">
+          <a href="https://twitter.com/gndx">
+            <img src="${image}" />
+          </a>
+          <a href="https://github.com/gndx">
+            <img src="${image}" />
+          </a>
+          <a href="https://instagram.com/gndx">
+            <img src="${image}" />
+          </a>
+        </div>
+      </div>
+    </div>
+  `;
+  return view;
+};
+
+export default Template;
